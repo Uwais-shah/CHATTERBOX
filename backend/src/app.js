e76f39111165916e467d14fcc4a0b8f4c03a863c
Uwais-shah <142101435+Uwais-shah@users.noreply.github.com>
2024-09-04 14:49:57 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+    const connectionDb = await mongoose.connect("mongodb+srv://uwaisshah7777:Uwaisshah7@cluster0.gvz7z.mongodb.net/")
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
